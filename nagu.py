65db8e5ccdf588fea29047628d20396af7bf9ac6
manjulamanjumm <129741778+manjulamanjumm@users.noreply.github.com>
2023-04-11 23:40:10 -0700

    hello world

---
+++ nagu.py
@@ -0,0 +1,8 @@
+num1=100
+num2=500
+sum=num1+num2
+print("the addition of two integers is:",sum)
+str1="Govt,polytechnic"
+str2="mosalehalli"
+sum2=str1+str2
+print("the addition of two strings is:",sum2)
